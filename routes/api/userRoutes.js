09d801a64e8c1d12c57fac41d6e1f2f45673af1d
Austin W <auswaller@gmail.com>
2023-07-24 10:54:31 -0400

    updated userRoutes to add update and delete

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,12 +3,12 @@ const {
     getUsers,
     getSingleUser,
     createUser,
+    updateUser,
+    deleteUser,
   } = require('../../controllers/userController');
   
-  // /api/users
   router.route('/').get(getUsers).post(createUser);
   
-  // /api/users/:userId
-  router.route('/:userId').get(getSingleUser);
+  router.route('/:userId').get(getSingleUser).delete(deleteUser).put(updateUser);
   
   module.exports = router;
